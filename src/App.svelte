5a48e1edecdcb5d91eeb62588e07bce8c3ac3520
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-23 21:27:37 +0530

    extract event data

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,14 @@
   import Product from './Product.svelte';
 
 	export let appName;
+
+  function addToCart(event) {
+    console.log(event.detail.id); // we passed the data'{id: 'p1'}'
+  }
+
+  function deleteProduct(event) {
+    console.log(event.detail);  // we passed the data'p1'
+  }
 </script>
 
 <style>
@@ -12,6 +20,6 @@
 
 <h1 class="capitalize-it">{appName}</h1>
 <Product productTitle="A book" 
-  on:add-to-cart={() => alert('Add to cart')}
-  on:delete={() => alert('Delete')}
+  on:add-to-cart={addToCart}
+  on:delete={deleteProduct}
 />
